69889dec08707c7f811c68de416c53f69e72a27a
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-06-16 10:02:17 +0600

    404 modified

--- 404.php
+++ 404.php
@@ -18,7 +18,7 @@
 				<h1>404</h1>
 			</div>
 			<h2>Oops! Nothing was found</h2>
-			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable. <a href="index.php">Return to homepage</a></p>
+			<p>The resource requested could not be found on this server! <a href="index.php">Return to homepage</a></p>
 		</div>
 	</div>
 
